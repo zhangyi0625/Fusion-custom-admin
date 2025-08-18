1d0e7c6248eee74cd0c3a2334b3c6b014cf8d318
张易 <2522248305@qq.com>
2025-08-18 17:09:34 +0800

    refactor：remove the unused menu functions and service api

--- src/views/cabinManage/MskTask/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import CabinTaskTemplate from '../CabinTaskTemplate'
-// export default <CabinTaskTemplate carrier="MSK" setting={null} />
-
-const MskTaskTem: React.FC = () => {
-  return <CabinTaskTemplate carrier="MSK" setting={null} />
-}
-
-export default MskTaskTem
